757c9f1509695f7d2708f2d7073409dc087eae59
sakit333 <saishkulkarni>
2024-12-19 11:41:25 +0530

    Swagger Documentation

--- src/main/java/org/jsp/product_api/ProductApiApplication.java
+++ src/main/java/org/jsp/product_api/ProductApiApplication.java
@@ -3,7 +3,12 @@ package org.jsp.product_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Product Rest API",description = "Rest Api using spring boot for performing crud operations",version = "1.4.3",contact = @Contact(url = "https://www.github.com/saishkulkarni",name="Saish",email = "saishkulkarni7@gmail.com")))
 public class ProductApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/org/jsp/product_api/controller/ProductController.java
+++ src/main/java/org/jsp/product_api/controller/ProductController.java
@@ -15,67 +15,69 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class ProductController {
 	
 	@Autowired
 	ProductService service;
 	
-	//Saving One Product
+	@Operation(summary = "Saving One Product")
 	@PostMapping("/products")
 	public ResponseEntity<Object> saveProduct(@RequestBody Product product){
 		return service.saveProduct(product);
 	}
 	
-	//Save Multiple Products
+	@Operation(summary = "Save Multiple Products")
 	@PostMapping("/products/many")
 	public ResponseEntity<Object> saveProducts(@RequestBody List<Product> products){
 		return service.saveProducts(products);
 	}
 	
-	//Fetch All Products
+	@Operation(summary = "Fetch All Products")
 	@GetMapping("/products")
 	public ResponseEntity<Object> fetchAllProducts(){
 		return service.fetchAllProducts();
 	}
 	
-	//Fetch Product By Id
+	@Operation(summary = "Fetch Product By Id")
 	@GetMapping("/products/{id}")
 	public ResponseEntity<Object> fetchById(@PathVariable int id){
 		return service.fetchById(id);
 	}
 	
-	//Fetch Product By Name
+	@Operation(summary = "Fetch Product By Name")
 	@GetMapping("/products/name/{name}")
 	public ResponseEntity<Object> fetchByName(@PathVariable String name){
 		return service.fetchByName(name);
 	}
 	
-	//Fetch Products Price Greater Than
+	@Operation(summary = "Fetch Products Price Greater Than")
 	@GetMapping("/products/price/greater/{price}")
 	public ResponseEntity<Object> fetchByPriceGreater(@PathVariable double price){
 		return service.fetchByPriceGreater(price);
 	}
 	
-	//Fetch Products By Stock Between
+	@Operation(summary = "Fetch Products By Stock Between")
 	@GetMapping("/products/stock/{min}/{max}")
 	public ResponseEntity<Object> fetchByStockBetween(@PathVariable int min,@PathVariable int max){
 		return service.fetchByStockBetween(min,max);
 	}
 	
-	//Delete Product By Id
+	@Operation(summary = "Delete Product By Id")
 	@DeleteMapping("/products/{id}")
 	public ResponseEntity<Object> deleteById(@PathVariable int id){
 		return service.deleteById(id);
 	}
 	
-	//Update Product - PUT
+	@Operation(summary = "Update Product - PUT")
 	@PutMapping("/products")
 	public ResponseEntity<Object> updateRecord(@RequestBody Product product){
 		return service.updateProduct(product);
 	}
 	
-	//Update Product- PATCH
+	@Operation(summary = "Update Product- PATCH")
 	@PatchMapping("/products/{id}")
 	public ResponseEntity<Object> updateRecord(@PathVariable int id,@RequestBody Product product){
 		return service.updateProduct(id,product);
